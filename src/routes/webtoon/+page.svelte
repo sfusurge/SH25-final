8df15e2dab023515ac5459803bc3fc2b57074b2b
brendanshen24 <91840687+brendanshen24@users.noreply.github.com>
2025-09-19 11:17:25 -0700

    second webtoon

--- src/routes/webtoon/+page.svelte
+++ src/routes/webtoon/+page.svelte
@@ -14,10 +14,29 @@
             "/assets/webtoons/1/1.8.webp",
             "/assets/webtoons/1/1.9.webp",
         ],
+        [
+            "/assets/webtoons/2/2.1.png",
+            "/assets/webtoons/2/2.2.png",
+            "/assets/webtoons/2/2.3.png",
+            "/assets/webtoons/2/2.4.png",
+            "/assets/webtoons/2/2.5.png",
+            "/assets/webtoons/2/2.6.png",
+            "/assets/webtoons/2/2.7.png",
+            "/assets/webtoons/2/2.8.png",
+            "/assets/webtoons/2/2.9.png",
+            "/assets/webtoons/2/2.10.png",
+            "/assets/webtoons/2/2.11.png",
+            "/assets/webtoons/2/2.12.png",
+            "/assets/webtoons/2/2.12.png",
+            "/assets/webtoons/2/2.13.png",
+            "/assets/webtoons/2/2.14.png",
+            "/assets/webtoons/2/2.15.png"
+        ]
     ];
 
     const currentIdx = $derived.by(() => {
-        const idx = parseInt(page.url.hash);
+        console.log(parseInt(page.url.hash))
+        const idx = parseInt(page.url.hash.slice(1));
         if (!idx || isNaN(idx) || idx < 1 || idx > webtoons.length) {
             return 0;
         }
