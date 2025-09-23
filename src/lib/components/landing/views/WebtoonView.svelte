cca7880a4b188a53d3c6b85926c3009b9c41e3a5
goldentoaste <rayg2375@gmail.com>
2025-09-22 22:10:55 -0700

    updated comic usage

--- src/lib/components/landing/views/WebtoonView.svelte
+++ src/lib/components/landing/views/WebtoonView.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import ScalableFrame from "$lib/components/maze/UI/ScalableFrame.svelte";
+    import { onMount } from "svelte";
     import { global } from "../../../../routes/+layout.svelte";
+    import { updateComicUsage } from "$lib/firebase/api";
 
     interface Props {
         key: number | string;
@@ -8,11 +10,39 @@
     }
 
     const { key, imageUrls = [] }: Props = $props();
+
+    let scrollY = $state(0);
+    let scrollHeight = $state(9990);
+    let endOfPage = $derived(scrollHeight - scrollY < scrollHeight * 0.1);
+    function onscroll(e: Event) {
+        scrollY = (e.target as HTMLElement).scrollTop;
+        scrollHeight = (e.target as HTMLElement).scrollHeight;
+    }
+
+    onMount(() => {
+        const initialViewed = localStorage.getItem(`initialView-${key}`);
+        if (!initialViewed) {
+            localStorage.setItem(`initialView-${key}`, "saved");
+            updateComicUsage(`${key}`, "view");
+        }
+    });
+
+    $effect(() => {
+        if (endOfPage) {
+            const savedEnd = localStorage.getItem(`endofpage-${key}`);
+            if (!savedEnd) {
+                localStorage.setItem(`endofpage-${key}`, "saved");
+                updateComicUsage(`${key}`, "finish");
+            }
+        }
+    });
+
+    $inspect(scrollY, scrollHeight);
 </script>
 
 <div class="parent" class:mobile={global.mobile}>
     <ScalableFrame style="width:100%; max-width:600px;">
-        <div class="imgWrapper">
+        <div class="imgWrapper" {onscroll}>
             <div class="image-container">
                 {#each imageUrls as imageUrl, index}
                     <img src={imageUrl} alt="Image {index + 1}" />
@@ -23,7 +53,6 @@
 </div>
 
 <style>
-  
     .parent {
         flex: 1;
         min-width: 0;
